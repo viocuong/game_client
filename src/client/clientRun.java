13ba00fde1d40829c16f83ef00e6f80cb06b4f4d
viocuong <cuong1999ads@gmail.com>
2020-11-20 18:01:09 +0700

    up

---
+++ src/client/clientRun.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import client.controllers.Controller;
+
+/**
+ *
+ * @author cuongnv
+ */
+public class clientRun {
+    public static void main(String[] args) {
+        var con = new Controller();
+    }
+}
---
+++ src/client/controllers/Controller.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client.controllers;
+import Models.com.*;
+import java.net.Socket;
+import java.sql.*;
+import client.view.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author cuongnv
+ */
+public class Controller {
+    private Socket socketClient;
+    private String serverHost="localhost";
+    private Connection con;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    private int serverPort=8888;
+    private LoginView loginView;
+    private Game game;
+    
+    public Controller(){
+        loginView = new LoginView();
+        loginView.setVisible(true);
+        loginView.addListentBtnLogin(new listentBtnLogin());
+    }
+    public void getUserOnline(){
+        send(new Request("getUserOnline",(Object)"he"));
+    }
+    public class listentBtnLogin implements MouseListener{
+
+        @Override
+        public void mouseClicked(MouseEvent me){
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            String s = null;
+//            loginView.dispose();
+//            Home h =new Home("hello  dww");
+            //h.setLabel("hello");
+//            h.setVisible(true);
+//            h.setLabel("hello");
+            //openConnection(serverHost, serverPort);
+            User user = loginView.getUser();
+            
+            send(new Request("login",(Object) user));
+            s = receive();
+            //loginView.showMessage(s);
+            if(s.equals("thanh cong")){
+                loginView.dispose();
+                game = new Game();
+                getUserOnline();
+                
+                
+            }
+            else loginView.showMessage(s);
+            closeConnection();
+        }
+        @Override
+        public void mousePressed(MouseEvent me){
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent me){
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent me){
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        @Override
+        public void mouseExited(MouseEvent me){
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    public String receive(){
+        String res=null;
+        try {
+            ois = new ObjectInputStream(socketClient.getInputStream());
+            res = (String) ois.readObject();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return res;
+        
+    }
+    public void closeConnection(){
+        try {
+            socketClient.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void openConnection(String serverHost, int port){
+        try {
+            socketClient = new Socket(serverHost, port);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void send(Request request){
+        openConnection(serverHost, serverPort);
+        try {
+            this.oos = new ObjectOutputStream(socketClient.getOutputStream());
+            oos.writeObject(request);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+   
+}
